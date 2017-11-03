caccf937e50b4f69cfe93f9e967330fd970a6f22
sqltxt <sqltxt@qq.com>
2017-11-03 23:22:17 +0800

    TB伺服

---
+++ AutoRunTB.py
@@ -0,0 +1,39 @@
+import win32gui
+from win32.lib import win32con
+import time
+import win32process
+
+def handle_window(hwnd, extra):
+ if win32gui.IsWindowVisible(hwnd):
+     if '交易开拓者平台' in win32gui.GetWindowText(hwnd):
+         #print(hwnd)#10进制句柄
+         #print(hex(hwnd))#16进制
+         win32gui.PostMessage(hwnd,win32con.WM_CLOSE,0,0)
+         
+def Close_DialogBox():
+    time.sleep(5)
+    hd=win32gui.FindWindow('#32770','确认')
+    #print(type(hd))
+    #print(hex(hd))
+    hdc=win32gui.FindWindowEx(hd,0,'Button','是(&Y)')
+    #print(hex(hdc))
+    time.sleep(5)
+    win32gui.SendMessage(hdc,win32con.WM_ACTIVATE,win32con.WA_ACTIVE,0)#激活窗口
+    time.sleep(3)
+    win32gui.SendMessage(hdc,win32con.BM_CLICK,1,0)
+    '''
+    win32gui.SendMessage(hdc,win32con.WM_KEYDOWN,win32con.VK_SPACE,0)#压下空格
+    time.sleep(1)
+    win32gui.SendMessage(hdc,win32con.WM_KEYUP,win32con.VK_SPACE,0)#抬起空格
+    '''
+    
+def TB_Open():
+    handle = win32process.CreateProcess('C:/Users/Administrator/Documents/tbv5321_x64_portable/TradeBlazer.exe','',None,None,0,win32process.CREATE_NO_WINDOW,None,'C:/Users/Administrator/Documents/tbv5321_x64_portable/',win32process.STARTUPINFO())#打开TB,获得其句柄  
+    time.sleep(25)
+    
+if __name__=='__main__':
+    TB_Open()
+    time.sleep(15)
+    win32gui.EnumWindows(handle_window,None)
+    #time.sleep(1)
+    Close_DialogBox()
