492efb4c957220574954ad02b5703919ae57adca
sqltxt <sqltxt@qq.com>
2017-11-09 11:13:59 +0800

    流程状态数status+登录次数times

--- AutoRunTB.py
+++ AutoRunTB.py
@@ -22,9 +22,9 @@ ExpirationDate = '2017-11-11'
 username = 'sqltxt'
 password = 'iamanothwolf'
 
-def Log(m,text):
+def Log(text):
     f = open(mylog,'a')
-    f.write(m+'\t'+datetime.datetime.now().strftime('[%H:%M:%S]')+'\t'+text+'\n')
+    f.write(str(status)+'\t'+datetime.datetime.now().strftime('[%H:%M:%S]')+'\t'+text+'\n')
     f.close()
     
 def handle_window(hwnd,extra):#TB句柄
@@ -33,19 +33,26 @@ def handle_window(hwnd,extra):#TB句柄
             global TB
             TB = hwnd
 def Kill():
-    if time.ctime()[12:19] == "8:50:00" or time.ctime()[12:19] == "20:50:00":
+    global status
+    global times
+    times = 0 #登录次数清零
+    if int(time.strftime("%I%M%S")) == 85000:
         if "TradeBlazer.exe" in os.popen('tasklist /FI "IMAGENAME eq TradeBlazer.exe"').read():
             os.system('TASKKILL /F /IM TradeBlazer.exe')
             os.system('TASKKILL /F /IM TBDataCenter.exe')
-            Log('0','进程清零')
+            status = 0
+            Log('进程清零')
             time.sleep(5)
     
 def TBStar_TBLogin(un,pw):
+    global status
+    global times
     if "TradeBlazer.exe" not in os.popen('tasklist /FI "IMAGENAME eq TradeBlazer.exe"').read():
         #打开TB
         handle = win32process.CreateProcess(path+'TradeBlazer.exe','',None,None,0,win32process.CREATE_NO_WINDOW,None,path,win32process.STARTUPINFO())#打开TB,获得其句柄
         time.sleep(2)
-        Log('1','打开TB')
+        status = 1 
+        Log('打开TB')
         #数据重置
         win32gui.PostMessage(win32gui.FindWindowEx(win32gui.FindWindow('#32770',None),0,'Button','数据重置'),win32con.BM_CLICK,1,0)
         time.sleep(2)
@@ -57,64 +64,91 @@ def TBStar_TBLogin(un,pw):
         time.sleep(2)
         #登录框
         win32gui.SendMessage(win32gui.FindWindowEx(win32gui.FindWindowEx(win32gui.FindWindow('#32770',None),0,'ComboBox',None),0,'Edit',None),win32con.WM_SETTEXT,0,un)
-        time.sleep(2)
+        time.sleep(1)
         win32gui.SendMessage(win32gui.FindWindowEx(win32gui.FindWindow('#32770',None),0,'Edit',None),win32con.WM_SETTEXT,0,pw)
-        time.sleep(2)
+        time.sleep(1)
         win32gui.SendMessage(win32gui.FindWindowEx(win32gui.FindWindow('#32770',None),0,'Button','登录(&L)'),win32con.BM_CLICK,1,0)
-        Log('2','登录TB')
+        status = 2
+        times = 1
+        Log(str(times)+' 次登录')
         #TB句柄
         time.sleep(10)
         win32gui.EnumWindows(handle_window,'交易开拓者')
         time.sleep(25)
         
 def TradeStar():
-    win32gui.PostMessage(TB,win32con.WM_COMMAND, win32gui.GetMenuItemID(win32gui.GetSubMenu(win32gui.GetMenu(TB),0),22),0)#启动所有自动交易
-    Log('3','启动交易')
-    time.sleep(2)
+    global status
+    if status == 2 or status == 10:
+        win32gui.PostMessage(TB,win32con.WM_COMMAND, win32gui.GetMenuItemID(win32gui.GetSubMenu(win32gui.GetMenu(TB),0),22),0)#启动所有自动交易
+        status = 3
+        Log('启动交易')
+        time.sleep(2)
     
 def MonitorStar():
-    win32gui.PostMessage(TB,win32con.WM_COMMAND, win32gui.GetMenuItemID(win32gui.GetSubMenu(win32gui.GetMenu(TB),7),11),0)#监控器
-    Log('4','打开监控')
-    time.sleep(10)
+    global status
+    if (status == 3):
+        win32gui.PostMessage(TB,win32con.WM_COMMAND, win32gui.GetMenuItemID(win32gui.GetSubMenu(win32gui.GetMenu(TB),7),11),0)#监控器
+        #print (st)
+        status = 4
+        Log('打开监控')
+        time.sleep(10)
+    
     
 def SaveWorkSpace():
+    global status
     win32gui.PostMessage(TB,win32con.WM_COMMAND, win32gui.GetMenuItemID(win32gui.GetSubMenu(win32gui.GetMenu(TB),0),9),0)#保存所有工作区
-    Log('5','保存工作区')
+    status = 5
+    Log('保存工作区')
     
     time.sleep(2)
 def MonitorClose():
+    global status
     win32gui.PostMessage(win32gui.FindWindow('#32770','自动交易头寸监控器'),win32con.WM_CLOSE,0,0)
     time.sleep(1)
     win32gui.SendMessage(win32gui.FindWindowEx(win32gui.FindWindow('#32770','确认 '),0,'Button','是(&Y)'),win32con.WM_ACTIVATE,win32con.WA_ACTIVE,0)#激活窗口
     time.sleep(2)
     win32gui.SendMessage(win32gui.FindWindowEx(win32gui.FindWindow('#32770','确认 '),0,'Button','是(&Y)'),win32con.BM_CLICK,0,0)
-    Log('6','关闭监控')
+    status = 6
+    Log('关闭监控')
     time.sleep(2)
 
 def TradeStop():
+    global status
     win32gui.PostMessage(TB,win32con.WM_COMMAND, win32gui.GetMenuItemID(win32gui.GetSubMenu(win32gui.GetMenu(TB),0),23),0)#停止所有自动交易
-    Log('7','停止交易')
+    status = 7
+    Log('停止交易')
     time.sleep(2)
 
 def TBClose():
+    global status
+    global times
     #win32gui.PostMessage(TB,win32con.WM_COMMAND, win32gui.GetMenuItemID(win32gui.GetSubMenu(win32gui.GetMenu(TB),0),27),0)#退出程序(菜单方式）
     win32gui.PostMessage(TB,win32con.WM_CLOSE,0,0)
     time.sleep(2)
     win32gui.SendMessage(win32gui.FindWindowEx(win32gui.FindWindow('#32770','确认'),0,'Button','是(&Y)'),win32con.WM_ACTIVATE,win32con.WA_ACTIVE,0)#激活窗口
     time.sleep(2)
     win32gui.SendMessage(win32gui.FindWindowEx(win32gui.FindWindow('#32770','确认'),0,'Button','是(&Y)'),win32con.BM_CLICK,1,0)
-    Log('8','关闭TB')
+    status = 8
+    Log('关闭TB')
+   
+    times = 0#登录次数清零
+    status = 0#状态清零
     
 def AccountLogin_LoginFail():
+    global status
+    global times
     while 1:
         n = win32gui.SendMessage(win32gui.FindWindowEx(win32gui.FindWindowEx(win32gui.FindWindowEx(TB,win32gui.FindWindowEx(TB,0,'AfxControlBar110',None),'AfxControlBar110',None),0,None,'帐户管理'),0,'SysListView32',None),LVM_GETITEMCOUNT)
         if n <1:
-            Log('11','柜台关闭')
+            status = 11
+            Log('柜台关闭')
             win32gui.PostMessage(TB,win32con.WM_COMMAND, win32gui.GetMenuItemID(win32gui.GetSubMenu(win32gui.GetMenu(TB),7),17),0)#帐户登录
             win32gui.SendMessage(win32gui.FindWindowEx(win32gui.FindWindow('#32770','帐户登录'),0,'Button','登录(&L)'),win32con.WM_ACTIVATE,win32con.WA_ACTIVE,0)#激活窗口
             time.sleep(2)
             win32gui.SendMessage(win32gui.FindWindowEx(win32gui.FindWindow('#32770','帐户登录'),0,'Button','登录(&L)'),win32con.BM_CLICK,1,0)
-            Log('10','再次登录')
+            status = 10
+            times = times + 1
+            Log(str(times)+' 次登录')
             time.sleep(20)
         else:
             break;
@@ -123,19 +157,24 @@ def ShutdownR():
     import platform
     import os
     #import system
+    global status
     ver = platform.platform()
     if 'Windows-7' in ver:
         os.system('shutdown -r')
-        Log('20','周末重启系统')
+        status = 20
+        Log('周末重启系统')
         time.sleep(60)
     else:
-        Log('20','非WIN7系统周末不重启')
+        status = 20
+        Log('非WIN7系统周末不重启')
 
 def Rubber():
+    global status
     if username != 'sqltxt' or username != 'gentle':
         for fname in os.listdir(path):
             if 'LOG_' in fname:
-                Log('40','清扫')
+                status = 40
+                Log('清扫')
                 os.remove(path+fname)
     f = open(mylog,'a')
     f.write('\n')
@@ -150,12 +189,14 @@ def Expired():
         
 if __name__=='__main__':
     f = open(mylog,'a')
-    f.write('\t'+datetime.datetime.now().strftime('[%H:%M:%S]')+'\tAutoRunTB启动'+'\n')
     f.write('\n')
+    f.write('\t'+datetime.datetime.now().strftime('[%H:%M:%S]')+'\tAutoRunTB启动'+'\n')
     f.close()
+    status = 0
+    times = 0
     while 1:
         if datetime.datetime.now().weekday()<=5:
-            if (85000<=int(time.strftime("%I%M%S"))<= 95000):
+            if (85000<=int(time.strftime("%I%M%S"))<= 115000 and status == 0):
                 Kill()
                 TBStar_TBLogin(username,password)
                 Expired()
